2b8282d1fd2dd341f0798693b6f6a04367c877d5
GlassyWorld <15832827782@163.com>
2020-01-13 09:55:58 +0800

    doom-private

---
+++ Emacs/.doom.d/+org.el
@@ -0,0 +1,167 @@
+;;;  -*- lexical-binding: t; -*-
+
+;;
+;; Org-mode
+(after! org
+  (setq outline-blank-line nil
+        org-cycle-separator-lines 2
+        org-log-done 'time
+        org-directory "~/org"
+        org-agenda-files '("~/org/todo.org"
+                           "~/org/incubate.org"
+                           "~/org/projects.org"
+                           "~/sync/org/Inbox.org"
+                           "~/sync/org/Ideer.org"
+                           "~/sync/org/Intressen.org")
+        org-latex-caption-above nil
+        org-html-table-caption-above nil
+        org-ditaa-jar-path "~/.emacs.d/.local/straight/repos/org/contrib/scripts/ditaa.jar")
+  ;; Org-links to emails
+  (require 'ol-notmuch))
+
+  ;; 设置 bullet list
+  (setq org-bullets-bullet-list '("☰" "☷" "☯" "☭"))
+
+  ;; 设置 todo keywords
+  (setq org-todo-keywords
+        '((sequence "TODO" "HAND" "|" "DONE")))
+  
+;; 调试好久的颜色，效果超赞！todo keywords 增加背景色
+(setf org-todo-keyword-faces '(("TODO" . (:foreground "white" :background "#95A5A6" :weight bold))
+                               ("HAND" . (:foreground "white" :background "#2E8B57" :weight bold))
+                               ("DONE" . (:foreground "white" :background "#3498DB" :weight bold))))
+;; ;; Hooks
+;; (add-hook 'org-mode-hook (lambda ()
+;;                            (hl-line-mode -1)))
+;; (add-hook 'org-mode-hook (lambda ()
+;;                            (flycheck-mode -1))
+;;           (add-hook 'org-mode-hook #'doom-disable-line-numbers-h))
+;; 
+;; ;;
+;; ;; Links
+;; (after! org-compat
+;;           (defvar yt-iframe-format
+;;             ;; You may want to change your width and height.
+;;             (concat "<iframe width=\"1600\""
+;;                     " height=\"1000\""
+;;                     " src=\"https://www.youtube.com/embed/%s\""
+;;                     " frameborder=\"0\""
+;;                     " allowfullscreen>%s</iframe>"))
+;; (org-add-link-type
+;;  "yt"
+;;  (lambda (handle)
+;;    (browse-url
+;;     (concat "https://www.youtube.com/embed/"
+;;             handle)))
+;;  (lambda (path desc backend)
+;;    (cl-case backend
+;;      (html (format yt-iframe-format
+;;                    path (or desc "")))
+;;      (latex (format "\href{%s}{%s}"
+;;                     path (or desc "video")))))))
+;; 
+;; 
+;; ;;
+;; ;; Notes
+;; (use-package! org-noter
+;;   :after org
+;;   :config
+;;   (setq org-noter-always-create-frame nil
+;;         org-noter-auto-save-last-location t)
+;;   (map! :localleader
+;;         :map org-mode-map
+;;         (:prefix-map ("n" . "org-noter")
+;;           :desc "Open org-noter" :n "o" #'org-noter
+;;           :desc "Kill org-noter session" :n "k" #'org-noter-kill-session
+;;           :desc "Insert org-note" :n "i" #'org-noter-insert-note
+;;           :desc "Insert precise org-note" :n "p" #'org-noter-insert-precise-note
+;;           :desc "Sync current note" :n "." #'org-noter-sync-current-note
+;;           :desc "Sync next note" :n "]" #'org-noter-sync-next-note
+;;           :desc "Sync previous note" :n "[" #'org-noter-sync-prev-note)))
+;; 
+;; 
+;; ;;
+;; ;; LaTeX
+;; (after! org
+;;   (require 'ox-latex)
+;;   ;; Make it possible to reference code blocks, figures etc.
+;;   (require 'org-ref)
+;;   ;; Fontify source code, use minted package opposed to listings.
+;;   (setq org-latex-listings 'minted)
+;;   ;; Specify options section, newfloat to minted to be able to setup the listing
+;;   ;; environment for the code blocks properly
+;;   (add-to-list 'org-latex-packages-alist '("section,newfloat" "minted" t))
+;;   ;; Shell commands to run upon compilation
+;;   (setq org-latex-pdf-process
+;;         '("xelatex -shell-escape -interaction nonstopmode %f"
+;;           "xelatex -shell-escape -interaction nonstopmode %f"
+;;           "xelatex -shell-escape -interaction nonstopmode %f")))
+;; 
+;; ;;
+;; ;; Hugo
+;; (after! ox-hugo
+;;   (defun org-hugo-publish ()
+;;     "Publish changes to the blog."
+;;     (interactive)
+;;     (let ((default-directory "~/src/emacs-blog/"))
+;;       ;; Build the project
+;;       (shell-command "hugo")
+;;       (let ((default-directory (concat default-directory "public/"))
+;;             (commit-message (format "rebuilding site %s" (shell-command-to-string "echo -n $(date +%Y%m%d)"))))
+;;         ;; Add modified files
+;;         (apply #'call-process `("git" nil "*add-debug*" nil "add" "."))
+;;         ;; Create commit
+;;         (apply #'call-process `("git" nil "*commit-debug*" nil "commit" "-m" ,commit-message))
+;;         ;; Publish
+;;         (apply #'call-process `("git" nil "*push-debug*" nil "push" "origin" "master"))))))
+;; 
+;; 
+;; ;;
+;; ;; Jira
+;; (use-package! ejira
+;;   :init
+;;   (setq jiralib2-url              "https://jira.zenuity.com"
+;;         jiralib2-auth             'basic
+;;         jiralib2-user-login-name  "niklas.carlsson@zenuity.com"
+;;         jiralib2-token            nil
+;; 
+;;         ejira-org-directory       (expand-file-name "ejira" doom-etc-dir)
+;;         ejira-projects            '("DUDE")
+;; 
+;;         ejira-priorities-alist    '(("Highest" . ?A)
+;;                                     ("High"    . ?B)
+;;                                     ("Medium"  . ?C)
+;;                                     ("Low"     . ?D)
+;;                                     ("Lowest"  . ?E))
+;;         ejira-todo-states-alist   '(("To Do"       . 1)
+;;                                     ("In Progress" . 2)
+;;                                     ("Review"      . 3)
+;;                                     ("Done"        . 4)))
+;; 
+;;   :config
+;;   ;; Tries to auto-set custom fields by looking into /editmeta
+;;   ;; of an issue and an epic.
+;;   (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
+;; 
+;;   ;; They can also be set manually if autoconfigure is not used.
+;;   ;; (setq ejira-sprint-field       'customfield_10001
+;;   ;;       ejira-epic-field         'customfield_10002
+;;   ;;       ejira-epic-summary-field 'customfield_10004)
+;; 
+;;   ;; Customize sprint
+;;   (setq ejira-sprint-field 'customfield_10004
+;;         ejira-scrum-project "DUDE")
+;; 
+;;   ;; Agenda integration
+;;   (require 'ejira-agenda)
+;;   (add-to-list 'org-agenda-files ejira-org-directory)
+;; 
+;;   ;; Add an agenda view with customized sections based on sprint content
+;;   (org-add-agenda-custom-command
+;;    '("j" "JIRA issues"
+;;      ((ejira-jql "project = DUDE and status != Done and assignee = currentUser()"
+;;                  ((org-agenda-overriding-header "Assigned to me")))
+;;       (ejira-jql "project = DUDE and sprint = \"Rap-a-map\" and status != Done and assignee is EMPTY"
+;;                  ((org-agenda-overriding-header "Unassigned")))
+;;       (ejira-jql "project = DUDE and sprint = \"Rap-a-map\" and status != Done and assignee != currentUser()"
+;;                  ((org-agenda-overriding-header "Others")))))))
---
+++ Emacs/.doom.d/config.el
@@ -0,0 +1,107 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Clay"
+      user-mail-address "15832827782@163.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 15))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+;; (setq doom-theme 'doom-one)
+
+
+;; looks
+(setq doom-modeline-height 40
+      +doom-modeline-buffer-file-name-style 'relative-from-project
+      doom-theme 'doom-dracula
+      +workspaces-switch-project-function #'ignore)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+
+(setq display-line-numbers-type t)
+(setq confirm-kill-emacs nil)
+
+
+
+;; Show week numbers in calendar
+(setq calendar-week-start-day 1
+      calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'font-lock-function-name-face))
+
+
+
+
+;;
+;; Command Log
+(use-package! command-log-mode
+  :commands global-command-log-mode
+  :config
+  (setq command-log-mode-auto-show t
+        command-log-mode-open-log-turns-on-mode nil
+        command-log-mode-is-global t
+        command-log-mode-window-size 50))
+
+
+;;
+;; Auto-formatting
+;; (add-hook 'c++-mode-hook #'+format|enable-on-save)
+(add-hook 'before-save-hook (lambda ()  (when (eq major-mode 'python-mode) (lsp-format-buffer))))
+(setq show-trailing-whitespace nil)
+
+
+
+
+;;
+;; PDF
+(after! pdf-tools
+  ;; customize midnight-mode colors
+  (setq pdf-view-midnight-colors '("#FFFFFF" . "#000000"))
+  (map!
+   :map pdf-view-mode-map
+   :desc "pdf-occur" :nmi "C-s" #'pdf-occur))
+
+
+;;
+;; load other config files
+(load! "+org")
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
