6bd3247a6decd5019d259e9b6c59bdb0d7075ec8
GlassyWorld <15832827782@163.com>
2022-05-03 22:11:27 +0800

    nvim lua 配置文件

--- Vim/latexmkrc
+++
@@ -1,4 +0,0 @@
-$pdf_mode = 1;
-$pdflatex = "xelatex --shell-escape -src-specials -synctex=1 -interaction=nonstopmode %O %S";
-$recorder = 1;
-$pdf_previewer = "Zathura -reuse-instance -inverse-search -a %O %S";
---
+++ nvim/lua/basic/config.lua
@@ -0,0 +1,9 @@
+-- 背景透明
+vim.g.background_transparency = true
+-- 指定 undotree 缓存存放路径
+vim.g.undotree_dir = "~/.cache/nvim/undodir"
+
+vim.opt.termguicolors = true
+
+-- 指定代码片段存储路径，这个目录我们在第二章节的时候已经创建好了
+vim.g.vsnip_snippet_dir = "~/.config/nvim/snippet"
